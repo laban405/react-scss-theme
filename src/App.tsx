de1e195bad7e1f6ce442be4ce8665dd3b2a0d84c
labanmogire <lmogire@innovexsolutions.co.ke>
2022-04-16 19:56:20 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useLocalStorage from 'use-local-storage'
+import { EBButton } from 'eastbytes-ui'
 
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme={theme}>
+      <span>Easy React Dark Mode</span>
+      <button onClick={switchTheme}>Switch Theme</button>
+      <div className='code-wrapper'>
+        <code>Sample code </code>
+      </div>
+      <EBButton label={'Sample Button'} size='large' primary={false} backgroundColor='green' />
     </div>
   );
 }
